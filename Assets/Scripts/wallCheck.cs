1a7d5428f8823d46b4925fa97dd1e9d4a715a522
KaneShoma <kane.shoma37@gmail.com>
2021-01-30 02:00:02 +0900

    壁のスクリプト書きました(鐘ヶ江)

--- Assets/Scripts/wallCheck.cs
+++ Assets/Scripts/wallCheck.cs
@@ -6,9 +6,11 @@ public class wallCheck : MonoBehaviour
 {
     gMScript gMScript;
     [SerializeField]
-    bool Red = true;//名前検討中
-    bool Blue = true;//名前検討中
-    bool Green = true;//名前検討中
+    bool Red = false;//名前検討中
+    [SerializeField]
+    bool Blue = false;//名前検討中
+    [SerializeField]
+    bool Green = false;//名前検討中
 
     private Collider wall;
 
@@ -18,11 +20,38 @@ public class wallCheck : MonoBehaviour
     }
     void Update()
     {
-        if(Red && gMScript.backColorR)
+        wall.isTrigger = false;
+        if (gMScript.backR && gMScript.backG && gMScript.backB)//1
         {
-            wall.isTrigger = true;
-             Blue = false;//名前検討中
-             Green = false;//名前検討中
+            if (Red && Green && Blue) wall.isTrigger = true;
+        }
+        if (!gMScript.backR && gMScript.backG && gMScript.backB)//2
+        {
+            if (!Red && Green && Blue) wall.isTrigger = true;
+        }
+        if (gMScript.backR && !gMScript.backG && gMScript.backB)//3
+        {
+            if (Red && !Green && Blue) wall.isTrigger = true;
+        }
+        if (gMScript.backR && gMScript.backG && !gMScript.backB)//4
+        {
+            if (Red && Green && !Blue) wall.isTrigger = true;
+        }
+        if (!gMScript.backR && !gMScript.backG && gMScript.backB)//5
+        {
+            if (!Red && !Green && Blue) wall.isTrigger = true;
+        }
+        if (!gMScript.backR && gMScript.backG && !gMScript.backB)//6
+        {
+            if (!Red && Green && !Blue) wall.isTrigger = true;
+        }
+        if (gMScript.backR && !gMScript.backG && !gMScript.backB)//7
+        {
+            if (Red && !Green &&! Blue) wall.isTrigger = true;
+        }
+        if (!gMScript.backR && !gMScript.backG && !gMScript.backB)//8
+        {
+            if (!Red && !Green && !Blue) wall.isTrigger = true;
         }
     }
 }
