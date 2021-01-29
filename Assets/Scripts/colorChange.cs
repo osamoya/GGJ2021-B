75e132c7d3ed6e1c68de0e6bc69c8c132ade2df2
KaneShoma <kane.shoma37@gmail.com>
2021-01-29 23:28:41 +0900

    スイッチにあたった判定の変更を行いました(鐘ヶ江)

---
+++ Assets/Scripts/colorChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChange : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "RSwitch")
+        {
+            //gameManager.backColorR = true;
+        }
+
+        if (collision.gameObject.tag == "GSwitch")
+        {
+            //gameManager.backColorG = true;
+        }
+
+        if (collision.gameObject.tag == "BSwitch")
+        {
+            //gameManager.backColorB = true;
+        }
+    }        
+      
+    }
+
