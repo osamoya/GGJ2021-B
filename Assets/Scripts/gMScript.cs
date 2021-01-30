28c5a011833a0a05d11c0170874c50fab75a1e5b
osamoya <osamumoyamoyasii@yahoo.co.jp>
2021-01-30 15:51:32 +0900

    勝又：テンプレデバッグ用

--- Assets/Scripts/gMScript.cs
+++ Assets/Scripts/gMScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class gMScript : MonoBehaviour
 {
+    
     public static bool backR;//背景色にどの成分が入っているか
     public static bool backG;//全部入りで白(使わない説もある)
     public static bool backB;//全部falseで黒の予定
@@ -13,4 +14,21 @@ public class gMScript : MonoBehaviour
     public static bool plB;
     
     public static int gameResult;//ゲームの状況を取得する変数
+
+
+    //ここからくそコード
+    //デバッグ用消してもよい
+    [SerializeField]
+    private bool debugMode=false;//これをオンにして、デバッグスタート
+    [SerializeField]
+    private bool R, G, B;
+
+    private void Update()
+    {
+        if (debugMode)
+        {
+            backR = R;backG = G; backB = B;
+        }
+    }
+
 }
