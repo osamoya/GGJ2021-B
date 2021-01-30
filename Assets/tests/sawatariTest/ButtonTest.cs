3d237ad0a45379893ee1338baeef83280414d52e
UHOUHOUHOUHO <gurenagaoredare@gmail.com>
2021-01-30 18:01:50 +0900

    resultControlを編集しました。また、テスト用に自分のシーンとファイルに幾つか変更を加えました。(猿渡)

---
+++ Assets/tests/sawatariTest/ButtonTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Click(Button button)
+    {
+     
+            gMScript.gameResult = 1;
+    
+        
+    }
+}
