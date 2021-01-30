cf710cb96ba14d8286fdcb5f82377238d5321fa5
KaneShoma <kane.shoma37@gmail.com>
2021-01-30 15:18:53 +0900

    pl+色の変数をgMScriptに追加しました。

--- Assets/Scripts/gMScript.cs
+++ Assets/Scripts/gMScript.cs
@@ -7,4 +7,7 @@ public class gMScript : MonoBehaviour
     public static bool backR;//背景色にどの成分が入っているか
     public static bool backG;//全部入りで白(使わない説もある)
     public static bool backB;//全部falseで黒の予定
+    public static bool plR;//自キャラの色です
+    public static bool plG;
+    public static bool plB;
 }
