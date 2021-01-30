46cdc7e1665dae5a5260b44348f5a73d224f7967
KaneShoma <kane.shoma37@gmail.com>
2021-01-30 14:44:58 +0900

    自キャラの色変更に関するスクリプトをちょこっと書きました(鐘ヶ江)

---
+++ Assets/Scripts/playerColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Rrazer")
+        {
+            //if (gMScript.plR) { gMScript.plR = false; } else { gMScript.plR = true; }
+        }
+
+        if (collision.gameObject.tag == "Grazer")
+        {
+            //if (gMScript.plG) { gMScript.plG = false; } else { gMScript.plG = true; }
+        }
+
+        if (collision.gameObject.tag == "Brazer")
+        {
+            //if (gMScript.plB) { gMScript.plB = false; } else { gMScript.plB = true; }
+        }
+    }
+}
