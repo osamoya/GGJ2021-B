351f3869caeb4f13aea50ed0566b826b12073bbd
KaneShoma <kane.shoma37@gmail.com>
2021-01-30 18:13:38 +0900

    鐘ヶ江の変更

---
+++ Assets/Scripts/KanegaeDebug.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KanegaeDebug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("background R"+gMScript.backR+" G"+gMScript.backG+" B"+gMScript.backB);
+    }
+}
