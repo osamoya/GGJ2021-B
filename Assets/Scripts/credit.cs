1beb315848e66c93f5c8bbf7baac142324ac4e3e
NetherR <rin.12.0322@gmail.com>
2021-01-31 15:05:46 +0900

    クレジット

---
+++ Assets/Scripts/credit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class credit : MonoBehaviour
+{
+    private int delay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        delay = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delay--;
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return))
+        {
+            if(delay<0)SceneManager.LoadScene("TitleScene");
+        }
+    }
+}
